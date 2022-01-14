f012a523a8f0d30930ff3eb68311489280531787
Arttu Suikkanen <asuikkan@c1r5p13.hive.fi>
2022-01-14 15:42:39 +0200

    trying to create a functional map

--- map.c
+++ map.c
@@ -13,17 +13,60 @@
 #include "fillit.h"
 #include <stdio.h>
 
+int	*map_tetrimino(int square, unsigned short shape)
+{
+	int				*mapped_tetrimino;
+	int				i;
+	int				j;
+	int				flag;
+
+	mapped_tetrimino = (int *)malloc(sizeof(int) * square);
+	i = 0;
+	while (i < square)
+	{
+		mapped_tetrimino[i] = 0;
+		i++;
+	}
+	i = 16;
+	j = 0;
+	while (i-- >= 0)
+	{
+		flag = 1;
+		flag = flag << i;
+		if (shape & flag)
+		{
+			mapped_tetrimino[j] = mapped_tetrimino[j] | flag;
+		}
+		if (i % 4 == 0)
+			j++;
+	}
+	return (mapped_tetrimino);
+}
+
+int	*create_map(int square)
+{
+	int	*map;
+	int	i;
+
+	map = (int *)malloc(sizeof(int) * square);
+	i = 0;
+	while (i < square)
+	{
+		map[i] = 0;
+		i++;
+	}
+	return (map);
+}
+
 int	map(t_tetrimino *list)
 {
-	int				map[5] = {0,0,0,0,0};
-	unsigned int	flag;
-	int				square;
+	int			*map;
+	int			*mapped_tetrimino;
+	static int	square;
 
-	square = 2;
-	flag = 1;
-	map[0] = 
-	
-	if ()
+	square = 4;
+	map = create_map(square);
+	mapped_tetrimino = map_tetrimino(square, list->shape);
 	//printf("%d\n", list->shape);
 	return (1);
 }
