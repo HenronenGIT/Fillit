1b4729f2e722a528b53062796dc0806e1b84c87d
Arttu Suikkanen <asuikkan@c1r5p11.hive.fi>
2022-01-17 12:56:28 +0200

    Updated map.c

--- map.c
+++ map.c
@@ -27,9 +27,9 @@ int	*map_tetrimino(int square, unsigned short shape)
 		mapped_tetrimino[i] = 0;
 		i++;
 	}
-	i = 16;
+	i = 15;
 	j = 0;
-	while (i-- >= 0)
+	while (j < square)
 	{
 		flag = 1;
 		flag = flag << i;
@@ -37,8 +37,12 @@ int	*map_tetrimino(int square, unsigned short shape)
 		{
 			mapped_tetrimino[j] = mapped_tetrimino[j] | flag;
 		}
+		i--;
 		if (i % 4 == 0)
+		{
+			i = 15;
 			j++;
+		}
 	}
 	return (mapped_tetrimino);
 }
@@ -62,11 +66,48 @@ int	map(t_tetrimino *list)
 {
 	int			*map;
 	int			*mapped_tetrimino;
+	int			i;
+	int			j;
+	int			flag;
+	int			square_check;
+	int			block_placed;
 	static int	square;
 
 	square = 4;
 	map = create_map(square);
 	mapped_tetrimino = map_tetrimino(square, list->shape);
+	square_check = 1;
+	square_check = square_check << (15 - square);
+	i = 15;
+	j = 0;
+	while (j < square)
+	{
+		flag = 1;
+		flag = flag << i;
+		/*if (flag & square_check)
+		{
+			j = 0;
+			while (j < square)
+			{
+				map[j] = map[j] ^ mapped_tetrimino[j];
+				j++;
+			}
+		}*/
+		if ((flag & mapped_tetrimino[j]) && (!(map[j] & flag)))
+		{
+			map[j] = map[j] | flag;
+		}
+		else
+		{
+			j = 0;
+			while (j < square)
+			{
+				mapped_tetrimino[j] = mapped_tetrimino[j] >> 1;
+				j++;
+			}
+		}
+		i--;
+	}
 	//printf("%d\n", list->shape);
 	return (1);
 }
