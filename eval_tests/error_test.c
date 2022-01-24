8469fe6ffa14ecbbdfaf9a91f88ee776e6bfc804
Henri <henrimaronen@gmail.com>
2022-01-24 17:22:57 +0200

    error test.c fix

--- eval_tests/error_test.c
+++ eval_tests/error_test.c
@@ -53,7 +53,7 @@ int main(void)
 		system("./fillit ./eval_tests/test_files/error_file/error_20");
 		system("./fillit ./eval_tests/test_files/error_file/error_21");
 		system("./fillit ./eval_tests/test_files/error_file/error_22");
-		system("./fillit ./eval_tests/test_files/error_file/error_23");
-		system("./fillit ./eval_tests/test_files/error_file/error_24");
-		system("./fillit ./eval_tests/test_files/error_file/error_25");
+		system("./fillit ./1eval_tests/test_files/error_file/error_23");
+		system("./fillit ./1eval_tests/test_files/error_file/error_24");
+		system("./fillit ./1eval_tests/test_files/error_file/error_25");
 }
