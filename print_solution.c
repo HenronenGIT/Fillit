2d9be355b8c9da6beb52655c08b98f115c3f1284
Henri <henrimaronen@gmail.com>
2022-01-25 14:41:52 +0200

    print_solution in norm, without malloc

--- print_solution.c
+++ print_solution.c
@@ -12,7 +12,7 @@
 
 #include "fillit.h"
 
-void	ft_bdot(void *s, int n)
+static void	ft_bdot(void *s, int n)
 {
 	int	i;
 
@@ -21,39 +21,44 @@ void	ft_bdot(void *s, int n)
 		((char *)s)[i] = '.';
 }
 
-int	print_solution(t_tetrimino *list, int side)
+static char	**allocate_map(int line_count, int line_len)
 {
-	char	**str;
-	int		line;
-	int		bit_index;
-	int		str_index;
+	char	**map;
 	int		i;
 
-	str_index = -1;
-	bit_index = 16;
-	line = -1;
 	i = -1;
-	str = ft_arrnew(side, side);
-	if (!str)
-		return (0);
-	while (++i != side)
-		ft_bdot((char *)str[i], side);
+	map = ft_arrnew(line_count, line_len);
+	if (!map)
+		return (NULL);
+	while (++i != line_count)
+		ft_bdot((char *)map[i], line_count);
+	return (map);
+}
+
+int	print_solution(t_tetrimino *list, int side)
+{
+	char	**map;
+	int		line;
+	int		bit_index;
+	int		char_index;
+
+	map = allocate_map(side, side);
 	while (list)
 	{
+		line = -1;
 		while (++line != side)
 		{
- 			while (++str_index != side && --bit_index)
+			char_index = -1;
+			bit_index = 16;
+			while (++char_index != side && --bit_index)
 			{
 				if (list->shape[line] & (1 << (bit_index)))
-					str[line][str_index] = 'A' + list->order;
+					map[line][char_index] = 'A' + list->order;
 			}
-			str_index = -1;
-			bit_index = 16;
 		}
-		line = -1;
 		list = list->next;
 	}
-	ft_print_array(str);
-	ft_free_2d_array(str);
+	ft_print_array(map);
+	ft_free_2d_array(map);
 	return (1);
 }
